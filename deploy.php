f4e12ec7fb34f957f2a18834acabffe5016af974
UdaraJay <devudara@gmail.com>
2019-10-10 18:20:01 -0400

    Add deployer

---
+++ deploy.php
@@ -0,0 +1,40 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'pr.lol');
+
+// Project repository
+set('repository', 'git@github.com:Pr-lol/pr.lol.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
+
+host('project.com')
+    ->set('deploy_path', '/var/www/html');
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
