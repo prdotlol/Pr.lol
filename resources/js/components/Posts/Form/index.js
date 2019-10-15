960f1948e8c896f7fa91e3c6b10fdec792ddb0bc
UdaraJay <devudara@gmail.com>
2019-10-15 17:36:14 -0400

    Add react forms

--- resources/js/components/Posts/Form/index.js
+++ resources/js/components/Posts/Form/index.js
@@ -1,45 +1,45 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ReactDOM from 'react-dom';
+import {useForm, useField} from '@shopify/react-form';
 
-const PostForm = (props) => {
-  const [hasError, setErrors] = useState(false);
-  const [post, setPost] = useState({});
+const PostForm = () => {
+    const {fields, submit, submitting, dirty, reset, submitErrors} = useForm({
+        fields: {
+            title: useField('some default title'),
+        },
+        onSubmit: fieldValues => {
+            return {status: 'fail', errors: [{message: 'bad form data'}]};
+        },
+        });
 
-  const fetchData = async () => {
-    const res = await axios.get(`/api/posts/${props.slug}`);
-    let { data } = res.data;
-    setPost(data);
-  }
+        const loading = submitting ? <p className="loading">loading...</p> : null;
+        const errors =
+        submitErrors.length > 0 ? (
+            <p className="error">{submitErrors.join(', ')}</p>
+        ) : null;
 
-  useEffect(() => {
-    fetchData()
-  }, [])
-
-  return (
-    <div className="react-single-post">
-        <h1 className="title">{post.title}</h1>
-        {post.images && post.images.map((image) => (
-          <img
-            key={image.id}
-            src={image.url}
-            className="featured-image"
-          />
-        ))}
-         {post.content &&post.content.split('\n').map(function(item, key) {
         return (
-            <span key={key}>
-            {item}
-            <br/>
-            </span>
-        )
-        })}
-    </div>
-  );
+        <form onSubmit={submit}>
+            {loading}
+            {errors}
+            <div>
+            <label htmlFor="title">
+                Title <input {...fields.title} />
+            </label>
+            </div>
+            <button type="button" disabled={!dirty} onClick={reset}>
+            Reset
+            </button>
+            <button type="submit" disabled={!dirty} onclick={submit}>
+            Submit
+            </button>
+        </form>
+        );
 };
 
 export default PostForm;
 
 $('*[data-react-render="post-form"]').each(function(i, obj) {
-    ReactDOM.render(<Post slug={obj.dataset.slug} />, obj);
+    ReactDOM.render(<PostForm slug={obj.dataset.slug} />, obj);
 });
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -11,60 +11,7 @@
             <div class="title">Share something...</div>
         </div>
         <div class="feed">
-            <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data">
-                @csrf
-                <div class="form-group">
-                    <label for="title">{{ __('Title') }}</label>
-                    <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required autofocus>
-                    @error('title')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-
-                <div class="form-group">
-                    <label for="type">Type</label>
-                    <select id="type" class="form-control">
-                        <option value="">1</option>
-                    </select>
-                    <small id="contentlHelp" class="form-text text-muted">Pick the most relevant type for this post.</small>
-                    @error('type')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-
-                <div class="form-group">
-                    <label for="image">{{ __('Image') }}</label>
-                    <input id="image" type="file" class="form-control-file @error('image') is-invalid @enderror" name="image" value="{{ old('image') }}" autofocus>
-                    <small id="contentlHelp" class="form-text text-muted">Select an image related to this post.</small>
-                    @error('image')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-
-                <div class="form-group">
-                    <label for="content">{{ __('Content') }}</label>
-                    <textarea id="content" type="text" class="form-control @error('content') is-invalid @enderror" name="content" value="{{ old('content') }}" required></textarea>
-                    <small id="contentlHelp" class="form-text text-muted">This is the main body of the post.</small>
-                    @error('content')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">
-                        {{ __('Publish Post') }}
-                    </button>
-                </div>
-            </form>
-
+           <div data-react-render="post-form"></div>
         </div>
         <div class="feed-sidebar">
             <div class="post-create-tips">
